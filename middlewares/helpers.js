f79a7802efeef3313748cbadbe6212d21edf5349
PalaciosZ <criszelaya24@hotmail.com>
2019-03-20 15:57:30 -0600

    Applying middlewares!

---
+++ middlewares/helpers.js
@@ -0,0 +1,8 @@
+
+exports.hasEmtyfields = function(req, res, next) {
+  const id = req.body.id;
+  if ( id === "") {
+        res.status(302).send({message:"Error, id empty"});
+  }
+  next();
+}
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 // allows to get information from post request
 const express = require("express");
 const routes = express.Router();
+const helpers = require("../middlewares/helpers")
 routes.use(express.json());
 routes.use(express.urlencoded({extended: true}));
 
-routes.post('/signup', (req, res) => {
+routes.post('/signup', helpers.hasEmtyfields, (req, res) => {
   const id = req.body.id;
   console.log(req.body.id);
   res.status(200).send({message:"Successfully welcome", id: id});
