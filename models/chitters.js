6796705c7208d43aaf801bcab3bcca138586c1c6
PalaciosZ <criszelaya24@hotmail.com>
2019-03-29 12:45:21 -0600

    Extracting chitters from DB

--- models/chitters.js
+++ models/chitters.js
@@ -1,3 +1,10 @@
+const db = require("../db/connection")
 exports.all = function(req, res) {
-  res.status(200).send({message: "missing queries!"})
+  db.query("SELECT * FROM chitters order by date DESC").then(function(data){
+    if (data.rowCount == 0) {
+      res.status(200).send({message: "Not chitters in DB"})
+      return
+    }
+    res.status(200).send({message: data.rows})
+  })
 }
