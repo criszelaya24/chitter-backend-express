fbfbcff71145c692cbcad08e2444f2c94b040adf
PalaciosZ <criszelaya24@hotmail.com>
2019-03-29 13:18:59 -0600

    Modifying query to chitter

--- models/chitters.js
+++ models/chitters.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection")
 exports.all = function(req, res) {
-  db.query("SELECT * FROM chitters order by date DESC").then(function(data){
+  db.query("select chitters.userId, users.username, chitters.title, chitters.body, chitters.date, chitters.id from users inner join chitters on users.id = chitters.userid order by date DESC").then(function(data){
     if (data.rowCount == 0) {
       res.status(200).send({message: "Not chitters in DB"})
       return
